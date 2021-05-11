d9917f4deed72fceef7d3378f48064f811a32532
Lucas Pancarte <lucas.pancarte@ubisoft.com>
2021-05-11 09:55:53 +0200

    Update Resizeable Window

--- Practice_Code/resizeable_frameless_window.py
+++ Practice_Code/resizeable_frameless_window.py
@@ -9,6 +9,170 @@ from PySide2.QtWidgets import *
 # from pymxs import runtime as rt
 
 
+"""
+
+class WindowResizer:
+
+    def __init__(self):
+        # __init__ vars used by resize_event_filter
+        self.press_control = 0
+        self.value = 0
+        self.origin = QPoint(0, 0)
+        self.ori_geo = self.geometry()
+
+    def resize_event_filter(self, obj, event):
+
+        # hover move event
+        if event.type() == QEvent.HoverMove:
+            if self.press_control == 0:
+                self.pos_control(event)
+
+        # mouse press event
+        if event.type() == QEvent.MouseButtonPress:
+            self.press_control = 1
+            self.origin = self.mapToGlobal(event.pos())
+            self.ori_geo = self.geometry()
+
+        # mouse release event
+        if event.type() == QEvent.MouseButtonRelease:
+            self.press_control = 0
+            self.pos_control(event)
+
+        # mouse move event
+        if event.type() == QEvent.MouseMove:
+            if self.cursor().shape() != Qt.ArrowCursor:
+                self.resizing(self.origin, event, self.ori_geo)
+
+        return True
+
+    def pos_control(self, e):
+        off = 15
+        rect = self.rect()
+        top_left = rect.topLeft()
+        top_right = rect.topRight()
+        bottom_left = rect.bottomLeft()
+        bottom_right = rect.bottomRight()
+        pos = e.pos()
+
+        # top resize rectangle
+        if QRect(QPoint(top_left.x() + off, top_left.y()), QPoint(top_right.x() - off, top_right.y() + off)).contains(pos):
+            self.setCursor(Qt.SizeVerCursor)
+            self.value = 1
+
+        # bottom resize rectangle
+        elif QRect(QPoint(bottom_left.x() + off, bottom_left.y()), QPoint(bottom_right.x() - off, bottom_right.y() - off)).contains(pos):
+            self.setCursor(Qt.SizeVerCursor)
+            self.value = 2
+
+        # right resize rectangle
+        elif QRect(QPoint(top_right.x() - off, top_right.y() + off), QPoint(bottom_right.x(), bottom_right.y() - off)).contains(pos):
+            self.setCursor(Qt.SizeHorCursor)
+            self.value = 3
+
+        # left resize rectangle
+        elif QRect(QPoint(top_left.x() + off, top_left.y() + off), QPoint(bottom_left.x(), bottom_left.y() - off)).contains(pos):
+            self.setCursor(Qt.SizeHorCursor)
+            self.value = 4
+
+        # top_right resize rectangle
+        elif QRect(QPoint(top_right.x(), top_right.y()), QPoint(top_right.x() - off, top_right.y() + off)).contains(pos):
+            self.setCursor(Qt.SizeBDiagCursor)
+            self.value = 5
+
+        # bottom_left resize rectangle
+        elif QRect(QPoint(bottom_left.x(), bottom_left.y()), QPoint(bottom_left.x() + off, bottom_left.y() - off)).contains(pos):
+            self.setCursor(Qt.SizeBDiagCursor)
+            self.value = 6
+
+        # top_left resize rectangle
+        elif QRect(QPoint(top_left.x(), top_left.y()), QPoint(top_left.x() + off, top_left.y() + off)).contains(pos):
+            self.setCursor(Qt.SizeFDiagCursor)
+            self.value = 7
+
+        # bottom_right resize rectangle
+        elif QRect(QPoint(bottom_right.x(), bottom_right.y()), QPoint(bottom_right.x() - off, bottom_right.y() - off)).contains(pos):
+            self.setCursor(Qt.SizeFDiagCursor)
+            self.value = 8
+
+        # default
+        else:
+            self.setCursor(Qt.ArrowCursor)
+
+    def resizing(self, ori, e, geo):
+        # top_resize
+        if self.value == 1:
+            last = self.mapToGlobal(e.pos()) - ori
+            first = geo.height() - last.y()
+            y = geo.y() + last.y()
+
+            if first > self.minimumHeight():
+                self.setGeometry(geo.x(), y, geo.width(), first)
+
+         # bottom_resize
+
+        # bottom_resize
+        elif self.value == 2:
+            last = self.mapToGlobal(e.pos()) - ori
+            first = geo.height() + last.y()
+            self.resize(geo.width(), first)
+
+        # right_resize
+        elif self.value == 3:
+            last = self.mapToGlobal(e.pos()) - ori
+            first = geo.width() + last.x()
+            self.resize(first, geo.height())
+
+        # left_resize
+        elif self.value == 4:
+            last = self.mapToGlobal(e.pos()) - ori
+            first = geo.width() - last.x()
+            x = geo.x() + last.x()
+
+            if first > self.minimumWidth():
+                self.setGeometry(x, geo.y(), first, geo.height())
+
+        # top_right_resize
+        elif self.value == 5:
+            last = self.mapToGlobal(e.pos()) - ori
+            first_width = geo.width() + last.x()
+            first_height = geo.height() - last.y()
+            first_y = geo.y() + last.y()
+
+            if first_height > self.minimumHeight():
+                self.setGeometry(geo.x(), first_y, first_width, first_height)
+
+        # bottom_right_resize
+        elif self.value == 6:
+            last = self.mapToGlobal(e.pos()) - ori
+            first_width = geo.width() - last.x()
+            first_height = geo.height() + last.y()
+            first_x = geo.x() + last.x()
+
+            if first_width > self.minimumWidth():
+                self.setGeometry(first_x, geo.y(), first_width, first_height)
+
+        # top_left_resize
+        elif self.value == 7:
+            last = self.mapToGlobal(e.pos()) - ori
+            first_width = geo.width() -  last.x()
+            first_height = geo.height() - last.y()
+            first_x = geo.x() +  last.x()
+            first_y = geo.y() + last.y()
+
+            if first_height > self.minimumHeight() and first_width > self.minimumWidth():
+                self.setGeometry(first_x, first_y, first_width, first_height)
+
+        # bottom_right_resize
+        elif self.value == 8:
+            last = self.mapToGlobal(e.pos()) - ori
+            first_width = geo.width() + last.x()
+            first_height = geo.height() + last.y()
+
+            self.setGeometry(geo.x(), geo.y(), first_width, first_height)
+
+"""
+
+
 class movable_label(QLabel):
     def __init__(self, parent):
         super(movable_label, self).__init__(parent)
@@ -52,27 +216,27 @@ class main(QMainWindow):
         self.setCentralWidget(self.central)
         self.resize(800, 500)
 
-    def eventFilter(self, obj, e):
+    def eventFilter(self, obj, event):
         # hover move event
-        if e.type() == QEvent.HoverMove:
+        if event.type() == QEvent.HoverMove:
             if self.press_control == 0:
-                self.pos_control(e)  # cursor position control for cursor shape setup
+                self.pos_control(event)
 
         # mouse press event
-        if e.type() == QEvent.MouseButtonPress:
+        if event.type() == QEvent.MouseButtonPress:
             self.press_control = 1
-            self.origin = self.mapToGlobal(e.pos())
+            self.origin = self.mapToGlobal(event.pos())
             self.ori_geo = self.geometry()
 
         # mouse release event
-        if e.type() == QEvent.MouseButtonRelease:
+        if event.type() == QEvent.MouseButtonRelease:
             self.press_control = 0
-            self.pos_control(e)
+            self.pos_control(event)
 
         # mouse move event
-        if e.type() == QEvent.MouseMove:
+        if event.type() == QEvent.MouseMove:
             if self.cursor().shape() != Qt.ArrowCursor:
-                self.resizing(self.origin, e, self.ori_geo, self.value)
+                self.resizing(self.origin, event, self.ori_geo)
 
         return True
 
