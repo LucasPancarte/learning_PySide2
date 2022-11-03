6537d2f4a62afa0b5ea745a93193d1bc1379a24d
Lucas Pancarte <pancarte.lucas@gmail.com>
2022-11-03 15:35:56 +0100

    Update Custom_CollapsableWidget2.py

--- Custom_Widgets/Custom_CollapsableWidget2.py
+++ Custom_Widgets/Custom_CollapsableWidget2.py
@@ -1,40 +1,44 @@
-from PySide2 import QtWidgets, QtGui
+from PySide2 import QtWidgets as qtw
+from PySide2 import qtg as qtg
+import MaxPlus
 
 
-class Header(QtWidgets.QWidget):
+class Header(qtw.QWidget):
     """Header class for collapsible group"""
 
     def __init__(self, name, content_widget):
         """Header Class Constructor to initialize the object.
         Args:
             name (str): Name for the header
-            content_widget (QtWidgets.QWidget): Widget containing child elements
+            content_widget (qtw.QWidget): Widget containing child elements
         """
         super(Header, self).__init__()
         self.content = content_widget
-        self.expand_ico = QtGui.QPixmap(":teDownArrow.png")
-        self.collapse_ico = QtGui.QPixmap(":teRightArrow.png")
+        self.expand_ico = qtg.QPixmap(":teDownArrow.png")
+        self.collapse_ico = qtg.QPixmap(":teRightArrow.png")
 
-        stacked = QtWidgets.QStackedLayout(self)
-        stacked.setStackingMode(QtWidgets.QStackedLayout.StackAll)
-        background = QtWidgets.QLabel()
+        self.setFixedHeight(20)
+
+        stacked = qtw.QStackedLayout(self)
+        stacked.setStackingMode(qtw.QStackedLayout.StackAll)
+        background = qtw.QLabel()
         background.setStyleSheet("QLabel{ background-color: rgb(93, 93, 93); border-radius:2px}")
 
-        widget = QtWidgets.QWidget()
-        layout = QtWidgets.QHBoxLayout(widget)
+        widget = qtw.QWidget()
+        layout = qtw.QHBoxLayout(widget)
 
-        self.icon = QtWidgets.QLabel()
+        self.icon = qtw.QLabel()
         self.icon.setPixmap(self.expand_ico)
         layout.addWidget(self.icon)
         layout.setContentsMargins(11, 0, 11, 0)
 
-        font = QtGui.QFont()
+        font = qtg.QFont()
         font.setBold(True)
-        label = QtWidgets.QLabel(name)
+        label = qtw.QLabel(name)
         label.setFont(font)
 
         layout.addWidget(label)
-        layout.addItem(QtWidgets.QSpacerItem(0, 0, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding))
+        layout.addItem(qtw.QSpacerItem(0, 0, qtw.QSizePolicy.Expanding, qtw.QSizePolicy.Expanding))
 
         stacked.addWidget(widget)
         stacked.addWidget(background)
@@ -53,28 +57,28 @@ class Header(QtWidgets.QWidget):
         self.icon.setPixmap(self.collapse_ico)
 
 
-class Container(QtWidgets.QWidget):
+class Container(qtw.QWidget):
     """Class for creating a collapsible group similar to how it is implement in Maya
         Examples:
             Simple example of how to add a Container to a QVBoxLayout and attach a QGridLayout
-            >>> layout = QtWidgets.QVBoxLayout()
+            >>> layout = qtw.QVBoxLayout()
             >>> container = Container("Group")
             >>> layout.addWidget(container)
-            >>> content_layout = QtWidgets.QGridLayout(container.contentWidget)
-            >>> content_layout.addWidget(QtWidgets.QPushButton("Button"))
+            >>> content_layout = qtw.QGridLayout(container.contentWidget)
+            >>> content_layout.addWidget(qtw.QPushButton("Button"))
     """
     def __init__(self, name, color_background=False):
+        super(Container, self).__init__()
         """Container Class Constructor to initialize the object
         Args:
             name (str): Name for the header
             color_background (bool): whether or not to color the background lighter like in maya
         """
-        super(Container, self).__init__()
-        layout = QtWidgets.QVBoxLayout(self)
+        layout = qtw.QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
-        self._content_widget = QtWidgets.QWidget()
+        self._content_widget = qtw.QWidget()
         if color_background:
-            self._content_widget.setStyleSheet(".QWidget{background-color: rgb(73, 73, 73); "
+            self._content_widget.setStyleSheet("QWidget{background-color: rgb(73, 73, 73); "
                                                "margin-left: 2px; margin-right: 2px}")
         header = Header(name, self._content_widget)
         layout.addWidget(header)
@@ -91,3 +95,17 @@ class Container(QtWidgets.QWidget):
         Returns: Content widget
         """
         return self._content_widget
+
+
+if __name__ == "__main__":
+
+    main_widget = qtw.QDialog(MaxPlus.GetQMaxMainWindow())
+    layout = qtw.QVBoxLayout(main_widget)
+    container = Container("Group", False)
+    layout.addWidget(container)
+
+    content_layout = qtw.QGridLayout(container.contentWidget)
+    content_layout.addWidget(qtw.QPushButton("Button"))
+    layout.addStretch()
+
+    main_widget.show()
